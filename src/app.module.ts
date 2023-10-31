0f0aa16cebb94c80bb88dfe938594d275f711c6e
DucHuy2801 <theflash28012002@gmail.com>
2023-11-01 00:37:51 +0700

    add mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { MongooseModule } from './mongoose/mongoose.module';
+import config from 'config';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    // ConfigModule.forRoot({
+    //   envFilePath: ['.env']
+    // }),
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: process.env.DB_HOST,
+    //   port: Number.parseInt(process.env.DB_PORT),
+    //   username: process.env.DB_USERNAME,
+    //   password: process.env.DB_PASSWORD,
+    //   database: process.env.DB_DATABASE,
+    //   entities: [],
+    //   synchronize: false
+    // })
     ConfigModule.forRoot({
-      envFilePath: ['.env']
+      envFilePath: ".env",
+      isGlobal: true
     }),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: Number.parseInt(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      entities: [],
-      synchronize: false
-    })
+    MongooseModule
   ],
   controllers: [AppController],
   providers: [AppService],
