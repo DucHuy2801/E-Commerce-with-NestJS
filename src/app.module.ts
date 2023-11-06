33929d36186d8c12e228f8f94bc4f3031e963780
DucHuy2801 <theflash28012002@gmail.com>
2023-11-06 11:04:31 +0700

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,31 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from './mongoose/mongoose.module';
-import config from 'config';
 import { ConfigModule } from '@nestjs/config';
+import { AllExceptionFilter } from './httpExceptionFilter';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    // ConfigModule.forRoot({
-    //   envFilePath: ['.env']
-    // }),
-    // TypeOrmModule.forRoot({
-    //   type: 'mysql',
-    //   host: process.env.DB_HOST,
-    //   port: Number.parseInt(process.env.DB_PORT),
-    //   username: process.env.DB_USERNAME,
-    //   password: process.env.DB_PASSWORD,
-    //   database: process.env.DB_DATABASE,
-    //   entities: [],
-    //   synchronize: false
-    // })
     ConfigModule.forRoot({
       envFilePath: ".env",
       isGlobal: true
     }),
-    MongooseModule
+    MongooseModule,
+    UsersModule
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'APP_FILTER',
+      useClass: AllExceptionFilter
+    }
+  ],
 })
 export class AppModule {}
