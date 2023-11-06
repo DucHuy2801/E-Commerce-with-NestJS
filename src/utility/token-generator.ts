e49b76a8b5229954a3d2aac58895b05cca656812
DucHuy2801 <theflash28012002@gmail.com>
2023-11-06 12:07:24 +0700

    update

--- src/utility/token-generator.ts
+++ src/utility/token-generator.ts
@@ -1,11 +1,11 @@
-import jwt from 'jsonwebtoken';
+import * as jwt from 'jsonwebtoken';
 
 export const generateAuthToken = (id: string) => {
-    return jwt.sign({_id: id}, process.env.jwtSecret, {
-        expiresIn: '30d'
-    })
-}
+  return jwt.sign({ id }, process.env.jwtSecret, {
+    expiresIn: '30d',
+  });
+};
 
 export const decodeAuthToken = (token: string) => {
-    return jwt.verify(token, process.env.jwtSercret)
-}
+  return jwt.verify(token, process.env.jwtSecret);
+};
